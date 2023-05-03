2e4ec029faccf6c6abfd28bd34f2a0218ccbf9d6
korrynversola <90277505+korrynversola@users.noreply.github.com>
2023-05-03 19:03:40 -0400

    not needed for deployment

--- recipes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Recipes (
-	recipeID INT AUTO_INCREMENT,
-	name TEXT NOT NULL,
-	description TEXT,
-	instructions LONGTEXT,
-	maxReadyTime TIME,
-	PRIMARY KEY (recipeID)	
-);
